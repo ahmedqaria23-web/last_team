368c5c78a4a362334a1b2056e1acb3d4ed2550a8
absdalluh <ahmedqaria23@gmail.com>
2026-07-26 14:40:32 -0900

    add condation

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 print("ahmed")
 print(" the team name 1 ahemd 2 abdalluh 3 eng abdalluh")
+
+name= "ahmed"
+
+if name == "ahmed ":
+    print(" your name is ahmed")
+    
+else:
+    print("what is your name ")
+        
